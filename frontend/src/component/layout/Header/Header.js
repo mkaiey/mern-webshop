90b70e791577fe3419c5dba4710716578c15a7c3
mkgit0000 <kaisarlord123@gmail.com>
2023-07-21 19:12:26 +0700

    update overlay-navbar, react-icons, fixed code

--- frontend/src/component/layout/Header/Header.js
+++ frontend/src/component/layout/Header/Header.js
@@ -1,45 +1,53 @@
 import React from "react";
 import { ReactNavbar } from "overlay-navbar";
 import logo from "../../../images/logo.png";
-
-const options = {
-  logo,
-  logoWidth: "20vmax",
-  burgerColor: "crimson",
-  navColor1: "white",
-  logoHoverSize: "10px",
-  burgerColorHover: "#900",
-  logoHoverColor: "crimson",
-  link1Text: "Home",
-  link2Text: "Products",
-  link3Text: "Contact",
-  link4Text: "About",
-  link1Url: "/",
-  link2Url: "/products",
-  link3Url: "/contact",
-  link4Url: "/about",
-  link1Size: "1.3vmax",
-  link1Color: "rgba(35, 35, 35,0.8)",
-  nav1justifyContent: "flex-end",
-  nav2justifyContent: "flex-end",
-  nav3justifyContent: "flex-start",
-  nav4justifyContent: "flex-start",
-  link1ColorHover: "#eb4034",
-  link1Margin: "1vmax",
-  profileIconUrl: "/login",
-  profileIconColor: "rgba(35, 35, 35,0.8)",
-  searchIconColor: "rgba(35, 35, 35,0.8)",
-  cartIconColor: "rgba(35, 35, 35,0.8)",
-  searchIconColorHover: "crimson",
-  cartIconColorHover: "crimson",
-  profileIconColorHover: "crimson",
-  searchIconMargin: "0.5vmax",
-  cartIconMargin: "1vmax",
-  profileIconMargin: "0.5vmax",
-};
+import { FaUserAlt } from "react-icons/fa";
+import { BiCartAlt } from "react-icons/bi";
+import { RiSearchLine } from "react-icons/ri";
 
 const Header = () => {
-  return <ReactNavbar {...options} />;
+  return (
+    <ReactNavbar
+      logo={logo}
+      logoWidth="20vmax"
+      navColor1="white"
+      burgerColor="crimson"
+      burgerColorHover="#900"
+      logoHoverColor="crimson"
+      nav1justifyContent="flex-end"
+      nav2justifyContent="flex-end"
+      nav3justifyContent="flex-start"
+      nav4justifyContent="flex-start"
+      link1Text="Home"
+      link2Text="Products"
+      link3Text="Contact"
+      link4Text="About"
+      link1Url="/"
+      link2Url="/products"
+      link3Url="/contact"
+      link4Url="/about"
+      link1Color="rgba(35, 35, 35,0.8)"
+      link1ColorHover="#eb4034"
+      link1Size="1.3vmax"
+      link1Margin="1vmax"
+      profileIcon={true}
+      searchIcon={true}
+      cartIcon={true}
+      ProfileIconElement={FaUserAlt}
+      SearchIconElement={RiSearchLine}
+      CartIconElement={BiCartAlt}
+      profileIconUrl="/login"
+      profileIconColor="rgba(35, 35, 35,0.8)"
+      profileIconColorHover="crimson"
+      searchIconColor="rgba(35, 35, 35,0.8)"
+      searchIconColorHover="crimson"
+      cartIconColor="rgba(35, 35, 35,0.8)"
+      cartIconColorHover="crimson"
+      searchIconMargin="0.5vmax"
+      cartIconMargin="1vmax"
+      profileIconMargin="0.5vmax"
+    />
+  );
 };
 
 export default Header;
